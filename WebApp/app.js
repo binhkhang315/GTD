fb93a18b0df521a174ddea1fee1aede06d36db68
Khang <binhkhang315@gmail.com>
2017-04-05 10:27:43 +0700

    create file to save data send from GPS module

--- WebApp/app.js
+++ WebApp/app.js
@@ -2,45 +2,23 @@ var express = require('express');
 var app = express();
 var serverIP = require('./serverIPAddress');
 var bodyParser = require('body-parser');
-var query  = require('url');
-var path = require('path');
+var fs = require('fs');
 
 app.use(bodyParser.json());
 // bodyParser.raw();
 app.use(bodyParser.urlencoded({
-     extended: false
+    extended: false
 }));
 
 var ipAddress = serverIP.getIP();
 
 app.set('view engine', 'jade');
-
 //register static directory
 app.use(express.static('public'));
 
-app.get('/index', function(req, res) {
-    res.sendFile('/index.html', {root: path.join(__dirname, 'public') })
-});
-console.log(path.join(__dirname,'public'))
-
-app.get('/location', function(req, res){
-    res.send({latitude: 106.100, longitude: 10.743813});
-});
-
-app.get('/test', function(req, res){
-    query.parse(req.url, true).query;
-    console.log("longitude: "+req.query.long);
-    console.log("latitude: "+ req.query.lat);
-    console.log("data: "+ req.query.data);
-    console.log("nhiet do: "+ req.query.nhietdo);
-    console.log("do am: "+ req.query.doam);
-    console.log("speed gps: "+ req.query.speedGPS);
-    console.log("time data: "+ req.query.time_data);
-
-    res.send({success: true})
-})
+app.use('/', require('./route/index.js'));
 
 app.set('port', (process.env.PORT || 3210));
-var server = app.listen(app.get('port'), ipAddress, function() {
-     console.log('Listening to:  ' + ipAddress +':'+app.get('port'));
+var server = app.listen(app.get('port'), ipAddress, function () {
+    console.log('Listening to:  ' + ipAddress + ':' + app.get('port'));
 });
---
+++ WebApp/route/index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+var query = require('url');
+var path = require('path');
+var fs = require('fs');
+
+router.get('/index', function (req, res) {
+    res.sendFile('index.html', { root: path.join(__dirname, 'public') })
+});
+
+router.get('/location', function (req, res) {
+    fs.appendFileSync('./log/abc.txt',"hello\n");
+    res.send({ latitude: 106.100, longitude: 10.743813 });
+});
+
+router.get('/updateData', function (req, res) {
+    //parse data in url to req.query;
+    query.parse(req.url, true).query;
+    //Get current date
+    var d = new Date();
+    //format date to String
+    var day = d.getDate().toString() + "-" + (d.getMonth() + 1).toString() + "-" + d.getFullYear().toString();
+    var time = d.getHours().toString() + ":" + d.getMinutes().toString() + ":" + d.getSeconds().toString();
+
+    //set file name
+    var fileNameGPS = day + "-GPS.txt";
+    var fileNameHistory = day + "-History.txt";
+    var fileNameDHT11 = day + "-DHT11.txt";
+    //Update GPS file
+    var gpsData = time + ", long: " + req.query.long.toString() + ", lat:" + req.query.lat.toString() + "\n";
+    fs.appendFile('./log/GPS/' + fileNameGPS, gpsData, (err)=>{
+        if(err) throw err;
+    });
+
+    //Update History file
+    var historyData = time + ", " + req.query.data.toString() + "\n";
+    fs.appendFile('./log/History/' + fileNameHistory, historyData, (err)=>{
+        if(err) throw err;
+    });
+
+    //Update DHT11 file
+    var dht11Data = time + ", nhiet do: " + req.query.nhietdo.toString() +
+        " doam: " + req.query.doam.toString() +
+        " speed GPS: " + req.query.speedGPS.toString() +
+        " time data: " + req.query.time_data.toString() + "\n";
+    fs.appendFile('./log/DHT11/' + fileNameDHT11, dht11Data, (err)=>{
+        if(err) throw err;
+    });
+    res.send({ success: true })
+});
+
+module.exports = router;
